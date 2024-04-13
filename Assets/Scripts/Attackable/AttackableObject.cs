afc52d8bc679e3d058f4ae898bf80696fcd1f8d9
Guillermina Manzano <manzanog0612@gmail.com>
2024-04-13 16:19:51 -0300

    Update AttackableObject.cs

--- Assets/Scripts/Attackable/AttackableObject.cs
+++ Assets/Scripts/Attackable/AttackableObject.cs
@@ -3,10 +3,18 @@ using UnityEngine;
 public class AttackableObject : MonoBehaviour, Attackable
 {
     private float health = 0;
+    private Team team = default;
+
+    public Team Team { get => team; }
 
     public void SetMaxHealth(float maxHealth)
     {
         health = maxHealth;
+    }
+
+    public void SetTeam(Team team)
+    {
+        this.team = team;
     }
 
     public virtual void ReceiveDamage(float damage)
