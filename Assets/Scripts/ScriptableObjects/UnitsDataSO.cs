783e615a7464985de2e2400c377fab04d53994aa
Ramaaa9461 <rami.guzmi@gmail.com>
2024-04-13 00:22:19 -0300

    Added UnitsData scriptableObject

---
+++ Assets/Scripts/ScriptableObjects/UnitsDataSO.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "UnitsData", menuName = "ScriptableObjects/UnitsDataSO", order = 1)]
+public class UnitsDataSO : ScriptableObject
+{
+    public string unitsName;
+    public int cost;
+
+    public float movementSpeed;
+
+    public float maxHealth;
+
+    public float damage;
+    public float attackSpeed;
+    public float range;
+
+    public AttackType attackType;
+
+    public GameObject unitPrefab;
+    
+    public AudioClip invocationSound;
+    public AudioClip attackSound;
+
+    //Se puede agregar particulas de Ataque/Invocacion/Muerte
+
+   public static float spawnCooldown;
+}
