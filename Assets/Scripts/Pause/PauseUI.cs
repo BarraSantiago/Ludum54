3cebbd16bb345d40d70b03cdba8921e765c28503
Mauro <mmendoza@wizards.us>
2024-04-14 22:51:02 -0300

    Update pause screen view

--- Assets/Scripts/Pause/PauseUI.cs
+++ Assets/Scripts/Pause/PauseUI.cs
@@ -5,15 +5,15 @@ using UnityEngine.UI;
 public class PauseUI : MonoBehaviour
 {
     [SerializeField] private GameObject holder = null;
+    [SerializeField] private Button continueBtn = null;
     [SerializeField] private Button retryBtn = null;
     [SerializeField] private Button menuBtn = null;
-    [SerializeField] private Button exitPauseBtn = null;
 
     private void Start()
     {
+        continueBtn.onClick.AddListener(Continue);
         retryBtn.onClick.AddListener(Retry);
         menuBtn.onClick.AddListener(BackToMenu);
-        exitPauseBtn.onClick.AddListener(Continue);
     }
 
     public void TogglePause(bool status)
